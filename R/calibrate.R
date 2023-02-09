f86d6522314284bfed10c82616fb1379530716ae
Benjamin Skov Kaas-Hansen <epiben@hey.com>
2023-02-09 23:27:23 +0100

    Change logic of calibration [estimate smooth curve, don't optimise abs(target - metric_value)]

--- R/calibrate.R
+++ R/calibrate.R
@@ -40,9 +40,6 @@
 #' - `grid_res` the resolution of the grid on which the acquisition function is
 #' evaluated
 #' - `tol` tolerace, if `abs(target - metric <= tol`, the optimiser halts
-#' - `nudge_sd` if the optimiser lands on an already seen value, a Gaussian
-#' noise of the form N(0, nudge_sd) is added to move the optimiser to a new
-#' value. Most relevant with a very coarse grid and low tolerance.
 #' - `kappa` tunable hyperparamter for the LCB acquisition function
 #'
 #' @return An object of class `calibrated_trial_spec` (inheriting from
@@ -66,104 +63,88 @@ calibrate <- function(
     trial_spec,
     target = 0.05,
     search_range = c(0.5, 1),
-    acq_fun = "lcb",
     superior_only = TRUE,
     inferiority = TRUE,
     cores = 1,
+    n_initial = 4,
+    n_iter_max = 25,
     n_rep = 1000,
+    grid_res = 10000,
+    grid_res_incr = 10,
+    grid_res_max = grid_res * 1000,
+    tol = 0.0001,
+    base_seed = as.numeric(Sys.Date()),
     progress = NULL,
-    base_seed = NULL,
-    verbose = FALSE,
-    controls = list(
-      n_initial = 4,
-      n_max = 25,
-      grid_res = 10000,
-      tol = 0.001,
-      nudge_sd = 0.01,
-      kappa = 2
-    )
+    verbose = FALSE
 ) {
 
+  # Housekeeping
   assert_pkgs(c("GPfit", "ggplot2"))
 
-  if (!is.list(controls)) {
-    warning0("The controls argument must be a list but wasn't: falling back to default")
-    controls <- formals()$controls
+  # TODO: not sure this functionality is useful
+  if (n_iter_max %% length(n_rep) != 0) {
+    stop0("Ensure that n_iter_max is a multiple of n_rep if n_rep is not a scalar.")
+  } else {
+    n_rep <- rep(n_rep, each = n_iter_max / length(n_rep))
   }
-  # Enforce defaults in control argument if unspecified
-  controls$n_initial <- controls$n_initial %||% 4
-  controls$n_max <- controls$n_max %||% 25
-  controls$grid_res <- controls$grid_res %||% 10000
-  controls$tol <- controls$tol %||% 0.001
-  controls$nudge_sd <- controls$nudge_sd %||% 0.01
-  controls$kappa <- controls$kappa %||% 2
+
+  # Helpers
+  to_original_scale <- function(x) {
+    x * diff(search_range) + search_range[1]
+  }
 
   # Expensive-to-evaluate function to approximate with GP
-  f <- function(thres) {
-    thres <- thres * diff(search_range) + search_range[1] # scale to natural scale
-    trial_spec$superiority <- thres
-    if (isTRUE(inferiority)) trial_spec$inferiority <- 1 - thres
+  f <- function(thres, n_rep = NULL) {
+    tmp_trial_spec <- trial_spec
+    tmp_trial_spec$superiority <- to_original_scale(thres)
+    if (isTRUE(inferiority)) {
+      tmp_trial_spec$inferiority <- 1 - tmp_trial_spec$superiority
+    }
 
     try(
-      {sims <- run_trials(trial_spec, n_rep = n_rep, cores = cores, base_seed = base_seed, progress = progress)},
+      sims <- run_trials(
+        tmp_trial_spec,
+        n_rep = n_rep,
+        cores = cores,
+        base_seed = base_seed,
+        progress = progress
+      ),
       silent = TRUE
     )
-    if (!exists("sims", inherits = FALSE)) return(1 - target)
+    if (!exists("sims", inherits = FALSE)) return(1)
 
     estimates <- check_performance(sims)
-    return(abs(target - estimates$est[estimates$metric == metric]))
-  }
-
-  # Helper functions
-  to_original_scale <- function(x, n_digits = ceiling(log10(controls$grid_res))) {
-    # Put back to original scale, round according to grid resolution
-    round(x * diff(search_range) + search_range[1], n_digits)
-  }
-
-  # Probability of improvement -- higher is better
-  poi <- function(mu, sigma) {
-    ifelse(sigma == 0, 0, pnorm((y_best - mu) / sigma)) # = poi
-    # 1 - poi (if maximising instead of minimising)
-  }
-
-  # Expected improvement -- higher is better
-  ei <- function(mu, sigma) {
-    gamma <- ifelse(sigma == 0, 0, (y_best - mu) / sigma)
-    phi <- pnorm(gamma)
-    sigma * (gamma * phi + dnorm(gamma))
-  }
-
-  # Lower confidence bound -- lower is better
-  lcb <- function(mu, sigma) {
-    mu - controls$kappa * sigma
+    return(estimates$est[estimates$metric == metric])
   }
 
   # Setup
-  if (isTRUE(verbose)) message("Setting up initial grid evaluation")
-
-  pred_x <- seq(0, 1, length.out = controls$grid_res)
-  metric <- if (superior_only) "prob_superior" else "prob_conclusive"
-
-  evaluations <- data.frame(y = numeric(0), x = numeric(0))
-  for (x in seq(0, 1, length.out = controls$n_initial)) {
-    if (isTRUE(verbose)) message("Evaluating at initial grid point")
-    evaluations <- rbind(evaluations, data.frame(y = f(x), x = x))
+  if (isTRUE(verbose)) {
+    message("Setting up initial grid evaluation")
   }
 
-  surr_plots <- list()
-  acq_plots <- list()
+  metric <- if (isTRUE(superior_only)) "prob_superior" else "prob_conclusive"
+
+  evaluations <- data.frame(y = numeric(0), x = numeric(0))
+  for (x in seq(0, 1, length.out = n_initial)) {
+    if (isTRUE(verbose)) {
+      message("Evaluating at initial grid point")
+    }
+
+    evaluations <- rbind(evaluations, data.frame(y = f(x, n_rep[1]), x = x))
+  }
+
+  surr_plots <- list() # plots of the surrogat function (with evaluated points)
 
   # Optimise w.r.t. chosen metric and target
-  for (n in seq_len(controls$n_max)) {
-    y_best <- min(evaluations$y) # best value so far
+  for (n in seq_len(n_iter_max)) {
+    best_idx <- which.min(abs(evaluations$y - target))
 
     if (isTRUE(verbose)) {
       message(sprintf(
-        "Iteration no. %s. Best threshold: %s. Best metric: %s +/- %s",
+        "Iteration no. %s. Best threshold so far: %s. Best metric value so far: %s",
         n,
-        to_original_scale(evaluations[which.min(evaluations$y), "x"]),
-        target,
-        round(min(evaluations$y), ceiling(-log10(controls$tol)))
+        to_original_scale(evaluations[best_idx, "x"]),
+        round(evaluations[best_idx, "y"], 1 + ceiling(-log10(tol)))
       ))
     }
 
@@ -173,24 +154,16 @@ calibrate <- function(
       corr = list(type = "exponential", power = 1.95)
     )
 
-    pred_df <- data.frame(x = union(pred_x, evaluations$x))
-      # ensure GP also predicts on nudged next_x values not on the defined grid
+    pred_df <- data.frame(x = seq(0, 1, length.out = grid_res))
     df <- as.data.frame(GPfit::predict.GP(fit, xnew = pred_df)$complete_data)
     colnames(df) <- c("x_grid", "mu", "mse")
     df$x_original_scale <- to_original_scale(df$x_grid)
     df$sigma <- sqrt(df$mse)
-    df$acq_value <- do.call(acq_fun, list(mu = df$mu, sigma = df$sigma))
 
-    which_fun <- ifelse(acq_fun == "lcb", which.min, which.max)
-    next_x_idx <- which_fun(rank(df$acq_value, ties.method = "random"))
-    next_x <- df[next_x_idx, "x_grid"]
+    next_x <- df[which.min(abs(df$mu - target)), "x_grid"]
+    next_x_natural <- to_original_scale(next_x)
 
-    while (next_x %in% evaluations$x) {
-      next_x <- rbeta(1, next_x * nrow(df), (1 - next_x) * nrow(df))
-        # beta distribution centered at next_x with mean/sample-size parameterisation
-    }
-
-    x_lab <- ifelse(isTRUE(superior_only), "Prob. of superiority", "Prob. of conclusiveness")
+    x_lab <- if (isTRUE(superior_only) "Prob. of superiority" else "Prob. of conclusiveness"
 
     surr_plots[[n]] <- ggplot2::ggplot(df, ggplot2::aes(x = x_original_scale)) +
       ggplot2::geom_ribbon(ggplot2::aes(ymin = mu - sigma, ymax = mu + sigma), alpha = 0.2) +
@@ -198,33 +171,61 @@ calibrate <- function(
       ggplot2::geom_point(ggplot2::aes(to_original_scale(x), y, colour = seq_along(x)),
                           evaluations, show.legend = FALSE) +
       ggplot2::scale_colour_gradient(low = "black", high = "red") +
-      ggplot2::geom_vline(xintercept = to_original_scale(next_x), linetype = 2, size = 0.2) +
+      ggplot2::geom_vline(xintercept = next_x_natural, linetype = 2, size = 0.2) +
+      ggplot2::geom_hline(yintercept = target, linetype = 2, size = 0.2) +
       ggplot2::labs(x = x_lab, y = NULL, title = "Approximate surrogate function") +
       ggplot2::theme_minimal()
 
-    acq_plots[[n]] <- ggplot2::ggplot(df, ggplot2::aes(x_original_scale, acq_value)) +
-      ggplot2::geom_line(size = 0.2) +
-      ggplot2::geom_vline(xintercept = to_original_scale(next_x), linetype = 2, size = 0.2) +
-      ggplot2::labs(x = x_lab, y = NULL, title = "Value of acquisition function") +
-      ggplot2::theme_minimal()
+    if (isTRUE(verbose)) {
+      print(surr_plots[[n]])
+    }
 
-    if (isTRUE(verbose)) print(surr_plots[[n]])
+    if (abs(evaluations[best_idx, "y"] - target) <= tol) {
+      if (isTRUE(verbose)) {
+        message(sprintf(
+          "=== Stopping because %s falls within %s +/- %s ===",
+          round(evaluations[best_idx, "y"], ceiling(-log10(tol))),
+          target,
+          tol
+        ))
+      }
+      break
+    }
 
-    if (y_best <= controls$tol) break
+    if (next_x %in% evaluations$x) {
+      if (grid_res >= grid_res_max){
+        if (isTRUE(verbose)) {
+          message(sprintf(
+            "Reached maximum grid resolution without finding a metric value ",
+            "within %s +/- %s (target +/- tol). Consider increasing grid_res_max",
+            target,
+            tol
+          ))
+        }
+        break
+      }
 
-    evaluations <- rbind(evaluations, c(f(next_x), next_x))
+      grid_res <- round(min(grid_res_max, grid_res * grid_res_incr))
+      if (isTRUE(verbose)) {
+        message(sprintf(
+          "Increasing grid resolution by a factor %s to %i",
+          grid_res_incr,
+          grid_res
+        ))
+      }
+    } else {
+      evaluations <- rbind(evaluations, c(f(next_x, n_rep[n]), next_x))
+    }
   }
 
-  evaluations$x <- to_original_scale(evaluations$x)
-  best_x <- evaluations[which.min(evaluations[, "y"]), "x"]
-
   # Update and extend trial_spec object
-  trial_spec$superiority <- best_x
-  if (isTRUE(inferiority)) trial_spec$inferiority <- 1 - best_x
+  evaluations$x <- to_original_scale(evaluations$x)
+
+  trial_spec$superiority <- evaluations[best_idx, "x"]
+  if (isTRUE(inferiority)) trial_spec$inferiority <- 1 - trial_spec$superiority
 
   trial_spec$calibration <- list(
     surrogate_plots = surr_plots,
-    acquisition_plots = acq_plots,
     evaluations = evaluations
   )
 
